5a4b66a42f6296fda725fe1105853bc13558a9b2
Andy Nicholson <andrew@anicholson.net>
2015-03-23 07:39:33 +1100

    Tweak the hard-coded values to something more like what I want the final result to be

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -93,8 +93,17 @@ kivaSnapshot.controller('PublicLoansController', function($scope) {
 
 
 kivaSnapshot.controller('NextLoanerController', function($scope) {
+  var loanerIsMe = function() {
+    if((Math.ceil(Math.random() * 100) % 2) == 0){
+      return 'me';
+    } else {
+      return '';
+    }
+  };
+
   $scope.loaner = {
-    name: "James Griffiths"
+    name: "James Griffiths",
+    is_me: loanerIsMe()
   };
 });
 
