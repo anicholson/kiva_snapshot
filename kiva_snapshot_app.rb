ab2de8589019da16bf7aca59a5ff01b46cbcd962
Andy Nicholson <andrew@anicholson.net>
2015-03-26 17:23:17 +1100

    Revert "…don't precompile?"
    This reverts commit f0e1541efd6f6e1726e93a393c68caf748729125.

--- kiva_snapshot_app.rb
+++ kiva_snapshot_app.rb
@@ -14,6 +14,8 @@ class KivaSnapshotApp < Sinatra::Base
 
   set :root, File.dirname(__FILE__)
 
+  set :assets_precompile, %w(*.js *.css *.png *.jpg *.svg *.eot *.ttf *.woff *.woff2)
+
   configure do
     mime_type :woff2, 'application/font-woff2'
   end
