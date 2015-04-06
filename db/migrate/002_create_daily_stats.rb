8be0c32a9c6def9d95a41e5cfe4d8fa072d6b187
Andy Nicholson <andrew@anicholson.net>
2015-04-06 17:41:35 +1000

    AR 4.2 needs decimal precision and places

--- db/migrate/002_create_daily_stats.rb
+++ db/migrate/002_create_daily_stats.rb
@@ -3,8 +3,8 @@ class CreateDailyStats < ActiveRecord::Migration
     create_table :daily_stats do |t|
       t.integer :loan_count
       t.integer :active_loan_count
-      t.decimal :amount_loaned
-      t.decimal :amount_repaid
+      t.decimal :amount_loaned, precision: 10, places: 2
+      t.decimal :amount_repaid, precision: 10, places: 2
       t.timestamps
     end
   end
