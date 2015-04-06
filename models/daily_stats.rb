13597a0a865c8494370103d716ed910d6fedebce
Andy Nicholson <andrew@anicholson.net>
2015-04-06 18:03:40 +1000

    Add #latest to DailyStats

--- models/daily_stats.rb
+++ models/daily_stats.rb
@@ -1,3 +1,19 @@
 class DailyStats < ActiveRecord::Base
 
+  def self.latest
+    order('created_at DESC').first || unavailable_daily_stats
+  end
+
+  class << self
+    private
+
+    def unavailable_daily_stats
+      OpenStruct.new(
+        loan_count: 0,
+        active_loan_count: 0,
+        amount_loaned: Float::NAN,
+        amount_repaid: Float::NAN
+      )
+    end
+  end
 end
