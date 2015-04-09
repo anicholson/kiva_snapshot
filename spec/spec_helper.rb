22fb522174c27c73f2c1b84c0cf9ed2428f58a27
Andy Nicholson <andrew@anicholson.net>
2015-04-10 08:36:07 +1000

    Add Capybara & use Poltergeist for JS tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
+# Always use test environment
 ENV['RACK_ENV'] ||= 'test'
 
+# Always see if the db's migrated before we start
 require 'sinatra/activerecord'
 require 'jdbc-postgresql'
 
@@ -8,7 +10,15 @@ ActiveRecord::Migration.maintain_test_schema!
 # Include the app code
 require_relative '../boot'
 
+# Keep the db clean
 require 'database_cleaner'
+
+# Capybara configuration
+require 'capybara'
+require 'capybara/poltergeist'
+
+Capybara.app = KivaSnapshotApp
+Capybara.javascript_driver = :poltergeist
 
 RSpec.configure do |config|
 
